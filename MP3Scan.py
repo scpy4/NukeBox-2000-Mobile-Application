069446b8cd44a0527f47699279961c8c214539bc
Superscurge <superscurge@gmail.com>
2016-05-21 05:39:44 +0100

    Documentation One

--- MP3Scan.py
+++ MP3Scan.py
@@ -16,7 +16,6 @@ from kivy.uix.scrollview import ScrollView
 
 from functools import partial
 import os
-
 import id3reader
 
 
@@ -31,110 +30,138 @@ class KivyMP3(GridLayout):
 
 	def MyID3(self, fileLoc):
 
+		# Create a scrollable container
 		self.scroll = ScrollView(do_scroll_x=(False))
+
+		# Asign the directory to a variable
 		self.musicDirectory = fileLoc
 		# Create the main wrapper
 		self.musicGrid = BoxLayout(orientation="vertical", size_hint=(1,1))
-		#self.bind(minimum_height=self.musicGrid.setter("height") ,minimum_width=self.musicGrid.setter("width"))
+		# Create a grid background to allign the buttons
 		xx = GridLayout(cols=1, size_hint=(1, None))
+		# Size fix for grid
 		xx.bind(minimum_height=xx.setter('height'), minimum_width=xx.setter('width'))
+		# Add the grid widget to the scroller 
 		self.scroll.add_widget(xx)
-		self.p = Label(text="Test")
+		# Artist label
+		self.p = Label()
+		# Song label
 		self.t = Label()
+		# Album label
 		self.a = Label()
+		# Begin the alternating count to alternate the background images (see bellow)
 		self.alternate = 1
 
+
+		# Process each file in the targeted directory 
 		for file in os.listdir(self.musicDirectory):
 
-			#self.temp = Button(text="tetsing",size_hint=(1,.2))
-
+			# Check if the file is an MP3
 			if file.endswith(".mp3"):
-				print(file)
 
+				# Run ID3 Reader to gather the ID3 data
 				id3r = id3reader.Reader(self.musicDirectory + file)
 
-				print "Artist: %s" % id3r.getValue('performer')
-				print "Album: %s" % id3r.getValue('album')
-				print "Track: %s" % id3r.getValue('title')
-
+				# Try to gather the artist ID3 data
 				try:
 					self.p.text = self.stripSquare(id3r.getValue('performer'))
 				except:
-					self.p.text = "None Found"
+					self.p.text = "Not Found"
+
+				# Try to gather the song title ID3 data
 				try:
 					self.t.text = self.stripSquare(id3r.getValue('title'))
 
 				except:
-					self.t.text = "None Found"
+					self.t.text = "Not Found"
+
+				# Try to gather the album ID3 data
 				try:
 					self.a.text = self.stripSquare(id3r.getValue('album'))
 				except:
-					self.a.text = "Non Found"
+					self.a.text = "Not Found"
 
 				
-				
+				# Create a spawn of the music button class
 				self.temp = MusicButton(text="testing", halign="center",
 										 size_hint=(1,None), height=("80dp"), 
 										 background_normal='images/listone.png',
 										 background_down='images/bk.png',
 										 markup=True)
 
+				# Set the music buttons filename attribute
 				self.temp.musicFileName = file
 
+				# Alternate the background colours
 				if self.alternate % 2 == 0:
-
-					self.temp.background_normal='images/listtwo.png'			 
-											 
+					self.temp.background_normal='images/listtwo.png'		 
 				self.alternate += 1
 
-
-				# Shorten the Title
+				# Shorten the Ssong title to prevent overflow.
 				self.t.text = self.t.text[:30] + (self.t.text[30:] and '..')
 
+				# Format the music button text
 				self.temp.text = "[anchor=title1][size=18sp][color=#4D5158]" + self.t.text + "[/color][/size]\n[color=#AAAAAA]" + self.p.text + "[/color]"
 				xx.add_widget(self.temp)
+
 				print "TEST: " + self.temp.text
+
+				# Link the button press and release to events
 				self.temp.bind(on_press=self.songSelect, on_release=self.songRelease)
 
 
 		# Return the newly created window
 		return self.scroll
 
-	# Actions
+	# Button press action
 	def songSelect(self,instance):
-
+		'''
+		Button - On Press display a change to acknowledge a registered PRESS
+		-	The button is also disabled at this stage to prevent spamming
+			the NukeBox 2000 server
+		'''
+		# Set the background colour on press
 		instance.background_color=(1,1,1,.9)
-		print "Clicked"
-		print instance.musicFileName
-		
+		# Disable the button on press - (Prevent spamming the server)
+		instance.disabled = True
+	
+	# Button release event	
 	def songRelease(self,instance):
+		'''
+		This simple chnages the background color of the button to acknowledge
+		a registered RELEASE.
+		'''
+		# Set the background colour on release
 		instance.background_color=(.9,.8,1,.8)
 
-
+	# Strip square brackets
 	def stripSquare(self,item):
 		'''
 		Remove square brackets from file names.
 		This prevents them breaking the format sequence.
-		(strip didn't work here for some reason, tried multiple combinations of 
+		(The strip option didn't work here for some reason, tried multiple combinations of 
 		    /[ /] [/]] etc.)
 		'''
+		# Remove left square bracket
 		item = item.replace('[',"")
+		# Remove right square bracket
 		item = item.replace(']',"")
+		# Return the text
 		return item
 
-
+# Application is run as main window
 if __name__ == '__main__':
 
-
+	# Build a Kivy application
     class MyDemo(App):
-
+    	# Define the main nwidget for the Kivy window
         def build(self):
 
             MyMain = KivyMP3().MyID3("sdcard/Download/")
             Window.size =(300,250)
 
+            # Return the contents to the window
             return MyMain
 
+    # Run the application
     MyDemo().run()
-
-
--- main.py
+++ main.py
@@ -189,8 +189,7 @@ class NukeBoxClientFactory(protocol.ClientFactory):
 		#reactor.stop()
 
 
-
-
+# Import Kivy Dependencies
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -213,128 +212,137 @@ from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
 
 from uuid import getnode as get_mac
 
-# A simple kivy App, with a textbox to enter messages, and
-# a large label to display all the messages received from
-# the server
+# Main application class
 class NukeBoxApp(App, KivyMP3):
+
+	'''
+	This is the main application class for the NukeBox 2000 mobile application.
+	-	This inherits the Kivys main app class and a custom KivyMP3 class which
+		provides a simpler way to view and interact with MP3's on a device.
+	'''
 	connection = None
 
+	# Built in Kivy Method
 	def build(self):
 
+		# Assign the setup method
 		root = self.setup_gui()
-
-
-		#self.connect_to_server(self.factory.server)
-		#Window.size =(432,768)
-
 
 		#Return the Window built inside setup_gui
 		return root	
 
+	# Define the setup method.
 	def setup_gui(self):
+		'''
+		The setup method is used to essentially build the applications.
+		-	Widgets are created and assigned to containers.
+		-	Layering of containers provides additional functionality such as the
+			spring feature when dragging down on the file browser
+		'''
 
-			# Check the operating system and set the file direttory prefix.
-			
-			# System is Linux
-			if platform == "linux":
-				self.prefix = "sdcard/"
+		# Check the operating system and set the file directory prefix.
+		
+		# System is Linux
+		if platform == "linux":
+			self.prefix = "sdcard/"
+			# set the window size to a mobile ratio
+			Window.size =(432,768)
 
-			# System is Android
-			else:
-				self.prefix = "/sdcard/"
+		# System is Android
+		else:
+			self.prefix = "/sdcard/"
 
-			# Get the MAC Address in hex format.
-			self.mac = hex(get_mac())
+		# Get the MAC Address in hex format.
+		self.mac = hex(get_mac())
 
-			# Create the main wrapper
-			self.wrap = GridLayout(cols=1, rows=4)
+		# Create the main wrapper
+		self.wrap = GridLayout(cols=1, rows=4)
 
-			# Create the Carosel
-			self.carousel = Carousel(direction='right',size_hint=(1,.7), loop="True",
-									anim_move_duration=(.1), anim_cancel_duration=(.1),min_move=(.1),scroll_distance=("20dp"),scroll_timeout=(100))
+		# Create the Carosel
+		self.carousel = Carousel(direction='right',size_hint=(1,.7), loop="True",
+								anim_move_duration=(.1), anim_cancel_duration=(.1),min_move=(.1),scroll_distance=("20dp"),scroll_timeout=(100))
 
-			# Carosel One
-			self.slideOne = BoxLayout(orientation="vertical", size_hint=(1,1))
-			self.scro = KivyMP3.MyID3(self,str(self.prefix)+"Download/")
-			self.slideOne.add_widget(self.scro)
-			self.carousel.add_widget(self.slideOne)
+		# Carosel One - Downloads Folder
+		self.slideOne = BoxLayout(orientation="vertical", size_hint=(1,1))
+		self.scro = KivyMP3.MyID3(self,str(self.prefix)+"Download/")
+		self.slideOne.add_widget(self.scro)
+		self.carousel.add_widget(self.slideOne)
 
-			# Carosel Two
-			self.scro2 = KivyMP3.MyID3(self,str(self.prefix)+"Music/")
-			self.slideTwo = BoxLayout(orientation="vertical")
-			self.carousel.add_widget(self.slideTwo)
-			self.slideTwo.add_widget(self.scro2)
+		# Carosel Two - Music Folder
+		self.scro2 = KivyMP3.MyID3(self,str(self.prefix)+"Music/")
+		self.slideTwo = BoxLayout(orientation="vertical")
+		self.carousel.add_widget(self.slideTwo)
+		self.slideTwo.add_widget(self.scro2)
 
-			# Create Header Image
-			header = BoxLayout(orientation="vertical",size_hint=(1,.1))
-			headerImage = Button(background_normal="./images/header.png", background_down="./images/header.png",
-									size_hint=(1,1))
-			header.add_widget(headerImage)
+		# Create Header Image
+		header = BoxLayout(orientation="vertical",size_hint=(1,.1))
+		headerImage = Button(background_normal="./images/header.png", background_down="./images/header.png",
+								size_hint=(1,1))
+		header.add_widget(headerImage)
 
-			# Create Header Buttons
-			headerBtns = BoxLayout (orientation="horizontal",size_hint=(1,.1))
-			headerBtnOne = Button(text="Downloads", size_hint=(.5,1),background_normal='images/tabone.png',background_down='images/tabtwo.png')
-			headerBtnTwo = Button(text="Music", size_hint=(.5,1),background_normal='images/tabone.png',background_down='images/tabtwo.png')
-			headerBtns.add_widget(headerBtnOne)
-			headerBtns.add_widget(headerBtnTwo)
+		# Create Header Buttons - (These Emlulate Tab Buttons)
+		headerBtns = BoxLayout (orientation="horizontal",size_hint=(1,.1))
+		headerBtnOne = Button(text="Downloads", size_hint=(.5,1),background_normal='images/tabone.png',background_down='images/tabtwo.png')
+		headerBtnTwo = Button(text="Music", size_hint=(.5,1),background_normal='images/tabone.png',background_down='images/tabtwo.png')
+		headerBtns.add_widget(headerBtnOne)
+		headerBtns.add_widget(headerBtnTwo)
 
-			self.label = Label(text='connecting...\n' + self.mac)
-			self.layout = BoxLayout(orientation='vertical',size_hint=(1,.5))
-			self.layout2 = BoxLayout(orientation='vertical',size_hint=(1,.5),padding=20)
+		#self.layout = BoxLayout(orientation='vertical',size_hint=(1,.5))
+		#self.layout2 = BoxLayout(orientation='vertical',size_hint=(1,.5),padding=20)
 
-			# Button Box
-			self.ButtonAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='bottom',size_hint=(1,.1))
-			self.ButtonLayout = BoxLayout(orientation="horizontal",size_hint=(1,1))
-			self.btn = Button(background_normal="./images/footer.png", background_down="./images/footer.png",
-				size_hint=(1,1))
+		# Button Box
+		self.ButtonAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='bottom',size_hint=(1,.1))
+		self.ButtonLayout = BoxLayout(orientation="horizontal",size_hint=(1,1))
+		self.btn = Button(background_normal="./images/footer.png", background_down="./images/footer.png",
+			size_hint=(1,1))
 
-			self.ButtonLayout.add_widget(self.btn)
-	
-			self.ButtonAnchorLayout.add_widget(self.ButtonLayout)
+		self.ButtonLayout.add_widget(self.btn)
 
-
-			# File Browser / Selection 
-
-			self.sel = FileChooserListView(rootpath=str(self.prefix)+"Download/",filters=["*.mp3"])
-			self.carousel.add_widget(self.sel)
-
-			self.sel.bind(on_submit=self.fileSelect)
-
-			# Add Items to main
-			self.wrap.add_widget(header)
-			self.wrap.add_widget(headerBtns)
-			# Add Carosel to Grid Layout
-			self.wrap.add_widget(self.carousel)
-			self.wrap.add_widget(self.ButtonAnchorLayout)
-			headerBtnOne.bind(on_press = partial(self.swTab,0))
-			headerBtnTwo.bind(on_press = partial(self.swTab,1))
+		self.ButtonAnchorLayout.add_widget(self.ButtonLayout)
 
 
+		# File Browser / Selection 
 
-			# Return the newly created window
-			return self.wrap
+		# While the KivyMP3 file overwrites this functionality I decided to
+		# 	leave it within the file as a third slide to provde access to
+		# 	non-traditional folders.
+
+		self.sel = FileChooserListView(rootpath=str(self.prefix)+"Download/",filters=["*.mp3"])
+		self.carousel.add_widget(self.sel)
+		self.sel.bind(on_submit=self.fileSelect)
+
+		# Add Items to main
+		self.wrap.add_widget(header)
+		self.wrap.add_widget(headerBtns)
+		# Add Carosel to Grid Layout
+		self.wrap.add_widget(self.carousel)
+		self.wrap.add_widget(self.ButtonAnchorLayout)
+
+		# Assign events to methods
+		headerBtnOne.bind(on_press = partial(self.swTab,0))
+		headerBtnTwo.bind(on_press = partial(self.swTab,1))
+
+		# Return the newly created window
+		return self.wrap
 
 	#=======================#
 	#		Events			#
 	#=======================#
 
 	def songSelect(self,instance):
+		'''
+		This builds upon the KivyMP3 method, introducing some custom
+		specifically for this application. 
+		'''
 
-		# Disable the button once pressed - Prevents multiple sends of the MP3
-		instance.disabled = True
-		print instance
-		print "Hellonn\n\n\n"
-		print instance.musicFileName
 		if self.carousel.index == 0:
 			fileDir = str(self.prefix)+"Download/" + str(instance.musicFileName)
 		elif self.carousel.index == 1:
 			fileDir = str(self.prefix)+"Music/" + str(instance.musicFileName)
 
-		# Call the Twisted transfer
+		# Call the Twisted network transfer method
 		self.twistIt(fileDir)
 
-	def songRelease(self,instance):
-		instance.background_color=(.9,.8,1,.8)
 
 	# Switch tabs
 	def swTab(self,slideValue,instance):
@@ -346,7 +354,7 @@ class NukeBoxApp(App, KivyMP3):
 			self.carousel.load_slide(self.carousel.slides[slideValue])
 
 
-
+	# Allow application pause/minimise on Android systems
 	def on_pause(self):
 		'''
 		This is required to keep the application in an "Alive state" while 
@@ -385,7 +393,7 @@ class NukeBoxApp(App, KivyMP3):
 		# Listen for the server
 		reactor.listenUDP(0, self.udp_protocol)
 
-	# Displat message on connectself.stripSquare
+	# Display message on connectself.stripSquare
 	def connect_to_server(self, ip):
 				
 		print 'Connect To Server Function ' + str(ip)
@@ -394,7 +402,6 @@ class NukeBoxApp(App, KivyMP3):
 		self.print_message("connected succesfully!")
 		self.connection = connection
 
-
-
+# Application is run as main window
 if __name__ == '__main__':
 	NukeBoxApp().run()
