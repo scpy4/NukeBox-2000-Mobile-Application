2d69f43a6c27f574d2400b10a6efbe90b7a301b5
Superscurge <Superscurge@gmail.com>
2016-05-22 07:36:42 +0100

    Final

--- MP3Scan.py
+++ MP3Scan.py
@@ -1,26 +1,31 @@
-#IMPORT ALL OF THE THINGS!
+# A00202022
+
+# Import Packages
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.anchorlayout import AnchorLayout #NB
+#from kivy.uix.anchorlayout import AnchorLayout #NB
 from kivy.core.window import Window
 from kivy.uix.button import Button
-from kivy.uix.image import AsyncImage
-from kivy.uix.image import Image
-from kivy.loader import Loader
+#from kivy.uix.image import AsyncImage
+#from kivy.uix.image import Image
+#from kivy.loader import Loader
 from kivy.uix.carousel import Carousel
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
+#from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
 from kivy.uix.scrollview import ScrollView
-
-from functools import partial
 import os
 import id3reader
 
 
 # Music button
 class MusicButton(Button):
+	'''
+	This serves as the music button
+	-	MusicButton inherits everything from the Kivy Button
+	-	The filename is added to the class
+	'''
 
 	# Additonal params - Music file name location
 	musicFileName = "myFile.mp3"
@@ -32,26 +37,30 @@ class KivyMP3(GridLayout):
 
 		# Create a scrollable container
 		self.scroll = ScrollView(do_scroll_x=(False))
-
+		
 		# Asign the directory to a variable
 		self.musicDirectory = fileLoc
-		# Create the main wrapper
-		self.musicGrid = BoxLayout(orientation="vertical", size_hint=(1,1))
+		
 		# Create a grid background to allign the buttons
-		xx = GridLayout(cols=1, size_hint=(1, None))
+		self.gridLayer = GridLayout(cols=1, size_hint=(1, None))
+		
 		# Size fix for grid
-		xx.bind(minimum_height=xx.setter('height'), minimum_width=xx.setter('width'))
+		self.gridLayer.bind(minimum_height=self.gridLayer.setter('height'), minimum_width=self.gridLayer.setter('width'))
+		
 		# Add the grid widget to the scroller 
-		self.scroll.add_widget(xx)
+		self.scroll.add_widget(self.gridLayer)
+		
 		# Artist label
-		self.p = Label()
+		self.artist = Label()
+		
 		# Song label
-		self.t = Label()
+		self.song = Label()
+		
 		# Album label
-		self.a = Label()
+		self.album = Label()
+		
 		# Begin the alternating count to alternate the background images (see bellow)
 		self.alternate = 1
-
 
 		# Process each file in the targeted directory 
 		for file in os.listdir(self.musicDirectory):
@@ -64,22 +73,22 @@ class KivyMP3(GridLayout):
 
 				# Try to gather the artist ID3 data
 				try:
-					self.p.text = self.stripSquare(id3r.getValue('performer'))
+					self.artist.text = self.stripSquare(id3r.getValue('performer'))
 				except:
-					self.p.text = "Not Found"
+					self.artist.text = "Not Found"
 
 				# Try to gather the song title ID3 data
 				try:
-					self.t.text = self.stripSquare(id3r.getValue('title'))
+					self.song.text = self.stripSquare(id3r.getValue('title'))
 
 				except:
-					self.t.text = "Not Found"
+					self.song.text = "Not Found"
 
 				# Try to gather the album ID3 data
 				try:
-					self.a.text = self.stripSquare(id3r.getValue('album'))
+					self.album.text = self.stripSquare(id3r.getValue('album'))
 				except:
-					self.a.text = "Not Found"
+					self.album.text = "Not Found"
 
 				
 				# Create a spawn of the music button class
@@ -98,13 +107,11 @@ class KivyMP3(GridLayout):
 				self.alternate += 1
 
 				# Shorten the Ssong title to prevent overflow.
-				self.t.text = self.t.text[:30] + (self.t.text[30:] and '..')
+				self.song.text = self.song.text[:30] + (self.song.text[30:] and '..')
 
 				# Format the music button text
-				self.temp.text = "[anchor=title1][size=18sp][color=#4D5158]" + self.t.text + "[/color][/size]\n[color=#AAAAAA]" + self.p.text + "[/color]"
-				xx.add_widget(self.temp)
-
-				print "TEST: " + self.temp.text
+				self.temp.text = "[anchor=title1][size=18sp][color=#4D5158]" + self.song.text + "[/color][/size]\n[color=#AAAAAA]" + self.artist.text + "[/color]"
+				self.gridLayer.add_widget(self.temp)
 
 				# Link the button press and release to events
 				self.temp.bind(on_press=self.songSelect, on_release=self.songRelease)
--- main.py
+++ main.py
@@ -1,195 +1,7 @@
+# A00202022
 
-import os.path
-import getpass
-import pickle
-
-#install_twisted_rector must be called before importing the reactor
-from kivy.support import install_twisted_reactor
-install_twisted_reactor()
-
-
-#A simple Client that send messages to the echo server
-from twisted.internet import reactor, protocol
-from socket import SOL_SOCKET, SO_BROADCAST
-from twisted.protocols.basic import LineReceiver
-
-#class EchoClient(protocol.Protocol):
-class NukeBoxClientProtocol(LineReceiver):
-
-	'''
-	NukeBox 2000 Client Protocol Class
-	'''
-
-	def __init__(self, factory, fname):
-
-		'''
-		Client Protocol constructor method
-		'''
-
-		# Create the Instance Variables
-
-		# Create a Reference to the Parent Factory Class
-		self.factory = factory
-
-		# Create the Reference to the File
-		self.fname = fname
-		#self.fname = "//sdcard/Download/Mike.mp3"
-
-		# Get the Users Name & Mac ID
-		self.name = getpass.getuser()
-		self.mac = hex(get_mac())
-
-	def connectionMade(self):
-
-		'''
-		Called when a connection is made
-		'''
-
-		# Get the Size of the File
-		filesize = os.path.getsize(self.fname)
-
-		# Serialize the Gathered Data & Send it to the Server
-		self.sendLine(pickle.dumps({"size": filesize,
-									"filename": self.fname,
-									"name": self.name,
-									'mac_id': self.mac}
-								   ))
-
-	def lineReceived(self, data):
-
-		'''
-		Called when a piece of data is received
-		'''
-
-		# If the Server Responds with a Request for Transfer, oblige
-		if data == 'tx':
-
-			# Open the File
-			f = open(self.fname, "rb")
-
-			# Read the File Contents and Send them to the Server
-			contents = f.read()
-			self.sendLine(contents)
-
-			# Close the File
-			f.close()
-
-		# Drop the Connection to the Server
-		else:
-			self.transport.loseConnection()
-
-	def connectionLost(self, reason):
-
-		'''
-		Called when the connection is lost
-		'''
-
-		print('Connection Lost')
-
-
-
-# Class to send a broadcast message and return the server 
-
-
-#class ClientBroadcastProtocol(protocol.DatagramProtocol):
-
-class NukeBoxClientBroadcastProtocol(protocol.DatagramProtocol):
-
-	'''
-	Implements a UDP Broadcaster to locate the NukeBox 2000 Server
-	'''
-
-	def __init__(self, factory):
-
-		'''
-		Client UDP Protocol constructor method
-		'''
-		# Create a reference to the Parent Factory
-		self.factory = factory
-
-	def startProtocol(self):
-
-		'''
-		Set the Socket for UDP Broadcast Packets
-		'''
-
-		# Set the Underlying Socket to UDP
-		self.transport.socket.setsockopt(SOL_SOCKET,
-										 SO_BROADCAST,
-										 True)
-
-		# Push out the UDP Packet
-		self.sendDatagram()
-
-	def sendDatagram(self):
-
-		'''
-		Transmit the UDP Discover Packet
-		'''
-
-		# Write the UDP Packet to the Transport
-		self.transport.write('Hello Jukebox',
-							 ('255.255.255.255', 19009)
-							 )
-
-	def datagramReceived(self, data, (ip, port)):
-
-		'''
-		Called when a UDP Response is received
-		- Uses the Responders address to make TCP Connection
-		'''
-
-		# Pull the Server IP Address from the Response & Make the Connection
-		self.factory.host = ip
-		self.factory.port = port
-		reactor.connectTCP(ip, 18008, self.factory)
-
-#class EchoFactory(protocol.Factory):
-class NukeBoxClientFactory(protocol.ClientFactory):
-
-	def __init__(self, fname):
-
-		'''
-		NukeBoxClient Factory constructor method
-		'''
-
-		# Create the Factory Instance Variables
-		# self.logger = log
-		self.fname = fname
-		self.host = ''
-
-	def buildProtocol(self, addr):
-
-		'''
-		Responsible for building Client Protocol Instances
-		One for each new client
-		'''
-
-		# Build an Instance of the Client Protocol
-		return NukeBoxClientProtocol(self, self.fname)
-
-	def clientConnectionFailed(self, connector, reason):
-
-		'''
-		Attempt to Reconnect when Connections Fail
-		'''
-
-		# Call the Connect Method on the Transport obj
-		connector.connect()
-
-	def clientConnectionLost(self, connector, reason):
-
-		'''
-		Lost Connections are Discarded
-		- Stops the Reactor Loop
-		'''
-
-		# Call Disconnect method on the Transport obj & Stop the reactor
-		connector.disconnect()
-		#reactor.stop()
-
-
-# Import Kivy Dependencies
+# Import Dependencies
+from kivyTwisted import *
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
@@ -203,13 +15,9 @@ from kivy.loader import Loader
 from kivy.uix.carousel import Carousel
 from kivy.uix.gridlayout import GridLayout
 from kivy.utils import platform
-
+from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
 from MP3Scan import KivyMP3
 from functools import partial
-
-from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
-
-
 from uuid import getnode as get_mac
 
 # Main application class
@@ -224,6 +32,9 @@ class NukeBoxApp(App, KivyMP3):
 
 	# Built in Kivy Method
 	def build(self):
+		'''
+		Build method is required by Kivy, it returns the main widget to the window.
+		'''
 
 		# Assign the setup method
 		root = self.setup_gui()
@@ -264,15 +75,15 @@ class NukeBoxApp(App, KivyMP3):
 
 		# Carosel One - Downloads Folder
 		self.slideOne = BoxLayout(orientation="vertical", size_hint=(1,1))
-		self.scro = KivyMP3.MyID3(self,str(self.prefix)+"Download/")
-		self.slideOne.add_widget(self.scro)
+		self.browserOne = KivyMP3.MyID3(self,str(self.prefix)+"Download/")
+		self.slideOne.add_widget(self.browserOne)
 		self.carousel.add_widget(self.slideOne)
 
 		# Carosel Two - Music Folder
-		self.scro2 = KivyMP3.MyID3(self,str(self.prefix)+"Music/")
+		self.browserTwo = KivyMP3.MyID3(self,str(self.prefix)+"Music/")
 		self.slideTwo = BoxLayout(orientation="vertical")
 		self.carousel.add_widget(self.slideTwo)
-		self.slideTwo.add_widget(self.scro2)
+		self.slideTwo.add_widget(self.browserTwo)
 
 		# Create Header Image
 		header = BoxLayout(orientation="vertical",size_hint=(1,.1))
@@ -287,17 +98,17 @@ class NukeBoxApp(App, KivyMP3):
 		headerBtns.add_widget(headerBtnOne)
 		headerBtns.add_widget(headerBtnTwo)
 
-		#self.layout = BoxLayout(orientation='vertical',size_hint=(1,.5))
-		#self.layout2 = BoxLayout(orientation='vertical',size_hint=(1,.5),padding=20)
-
 		# Button Box
+		
+		# Anchor to bottom of screen
 		self.ButtonAnchorLayout = AnchorLayout(anchor_x='center', anchor_y='bottom',size_hint=(1,.1))
+		
+		# Footer
 		self.ButtonLayout = BoxLayout(orientation="horizontal",size_hint=(1,1))
 		self.btn = Button(background_normal="./images/footer.png", background_down="./images/footer.png",
 			size_hint=(1,1))
-
+		# Footer layout
 		self.ButtonLayout.add_widget(self.btn)
-
 		self.ButtonAnchorLayout.add_widget(self.ButtonLayout)
 
 
@@ -307,13 +118,15 @@ class NukeBoxApp(App, KivyMP3):
 		# 	leave it within the file as a third slide to provde access to
 		# 	non-traditional folders.
 
-		self.sel = FileChooserListView(rootpath=str(self.prefix)+"Download/",filters=["*.mp3"])
-		self.carousel.add_widget(self.sel)
-		self.sel.bind(on_submit=self.fileSelect)
+		# Call the file browser - set the default path to the root and only view MP3s
+		self.fileBrowse = FileChooserListView(rootpath="/", filters=["*.mp3"])
+		self.carousel.add_widget(self.fileBrowse)
+		self.fileBrowse.bind(on_submit=self.fileSelect)
 
 		# Add Items to main
 		self.wrap.add_widget(header)
 		self.wrap.add_widget(headerBtns)
+		
 		# Add Carosel to Grid Layout
 		self.wrap.add_widget(self.carousel)
 		self.wrap.add_widget(self.ButtonAnchorLayout)
@@ -326,13 +139,16 @@ class NukeBoxApp(App, KivyMP3):
 		return self.wrap
 
 	#=======================#
-	#		Events			#
+	#		Methods			#
 	#=======================#
-
+	
+	# Song selection method
 	def songSelect(self,instance):
 		'''
 		This builds upon the KivyMP3 method, introducing some custom
-		specifically for this application. 
+		specifically for this application. The method takes the instance
+		value by default:
+		-	Instance value of the button being pressed.
 		'''
 
 		if self.carousel.index == 0:
@@ -345,47 +161,62 @@ class NukeBoxApp(App, KivyMP3):
 
 
 	# Switch tabs
-	def swTab(self,slideValue,instance):
+	def swTab(self, slideValue, instance):
 		'''
-		Switch file viewer depending if not on current
+		Switches the carousel depending on which slide is n view.
+		The method takes two additional attributes:
+		-	The target slide for the button press.
+		-	The instance of the item (Kivy)
 		'''
-
-		if self.carousel.index != slideValue:
-			self.carousel.load_slide(self.carousel.slides[slideValue])
+		
+		try:
+			if self.carousel.index != slideValue:
+				self.carousel.load_slide(self.carousel.slides[slideValue])
+		except:
+			pass
 
 
 	# Allow application pause/minimise on Android systems
 	def on_pause(self):
+		
 		'''
 		This is required to keep the application in an "Alive state" while 
 		minimized on Android systmes.
 		'''
+		
 		return True
 
 	#File Selected - Gather the path
-	def fileSelect(self,kivyObject,selectedDir,position):
+	def fileSelect(self, kivyObject, selectedDir, position):
 
 		'''
-		The object of this method is to gather the file information, once this has been established
-		the file is transfered over the network using twisted.
+		The object of this method is to gather the file information, 
+		once this has been established the file is transfered over the 
+		network using twisted. 
+		
+		The method takes 3 variables by default(Kivy):
+		-	The instance information.
+		-	The selected directory location.		
+		-	The positional informaiton
 		'''
 
-		# Process the directory
+		# Process the directory information
 		self.direct = selectedDir[0]
 		x = self.direct
 		x = self.direct.split("/")
-
-		# Print the file name 
-		print x[-1]
-
-		# Print the directory in full
 		self.direct = str(x[-1])
-		print ("File Directory: " + selectedDir[0])
-
+		
+		# Call Twisted
 		self.twistIt(selectedDir[0])
 
 	# Call twisted functionality
 	def twistIt(self,fileValue):
+		
+		'''
+		This method calls the Twisted functionality of the program.
+		-	The method takes onf additional value representing the files locaiton.
+		'''
+		
 		# Define the factory
 		self.factory = NukeBoxClientFactory(fileValue)
 		# Set up protocol
@@ -393,15 +224,8 @@ class NukeBoxApp(App, KivyMP3):
 		# Listen for the server
 		reactor.listenUDP(0, self.udp_protocol)
 
-	# Display message on connectself.stripSquare
-	def connect_to_server(self, ip):
-				
-		print 'Connect To Server Function ' + str(ip)
-
-	def on_connection(self, connection):
-		self.print_message("connected succesfully!")
-		self.connection = connection
 
 # Application is run as main window
 if __name__ == '__main__':
+	
 	NukeBoxApp().run()
